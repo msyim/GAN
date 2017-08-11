4927099c6ee37c2b3bc965e7ef8ac81d2c61f5b0
developer-account <irteam@d3b095.nhnsystem.com>
2017-08-11 17:48:07 +0900

    parameter tuning. activation fnc. changed

--- GAN.py
+++ GAN.py
@@ -1,57 +1,55 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+from matplotlib import pyplot as plt
 
 # Data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # HyperParameters
-batch_size = 64
+batch_size = 256
 z_size = 128
-epochs = 10
+h1_size = 256
+h2_size = 512
+epochs = 500
 K = 1
 
 X = tf.placeholder(tf.float32, shape=[None, 784])
+keep_prob = tf.placeholder(tf.float32)
 
 def weight_variable(name, shape):
   #return tf.get_variable(name, shape=shape, initializer=tf.truncated_normal_initializer())
   return tf.get_variable(name, shape=shape, initializer=tf.contrib.layers.xavier_initializer())
 
 weights = {
-  "DW1" : weight_variable("DW1", [784,256]),
-  "Db1" : weight_variable("Db1", [256]),
-  "DW2" : weight_variable("DW2", [256,128]),
-  "Db2" : weight_variable("Db2", [128]),
-  "DW3" : weight_variable("DW3", [128,32]),
-  "Db3" : weight_variable("Db3", [32]),
-  "DW4" : weight_variable("DW4", [32,1]),
-  "Db4" : weight_variable("Db4", [1]),
+  "DW1" : weight_variable("DW1", [784,h2_size]),
+  "Db1" : weight_variable("Db1", [h2_size]),
+  "DW2" : weight_variable("DW2", [h2_size,h1_size]),
+  "Db2" : weight_variable("Db2", [h1_size]),
+  "DW3" : weight_variable("DW3", [h1_size,1]),
+  "Db3" : weight_variable("Db4", [1]),
 
-  "GW1" : weight_variable("GW1", [128,256]),
-  "Gb1" : weight_variable("Gb1", [256]),
-  "GW2" : weight_variable("GW2", [256,512]),
-  "Gb2" : weight_variable("Gb2", [512]),
-  "GW3" : weight_variable("GW3", [512,784]),
+  "GW1" : weight_variable("GW1", [z_size,h1_size]),
+  "Gb1" : weight_variable("Gb1", [h1_size]),
+  "GW2" : weight_variable("GW2", [h1_size,h2_size]),
+  "Gb2" : weight_variable("Gb2", [h2_size]),
+  "GW3" : weight_variable("GW3", [h2_size,784]),
   "Gb3" : weight_variable("Gb3", [784])
 }
 
 def Discriminator(data_merged):
-  hidden1 = tf.nn.relu(tf.matmul(data_merged, weights["DW1"]) + weights["Db1"])
-  hidden2 = tf.nn.relu(tf.matmul(hidden1, weights["DW2"]) + weights["Db2"])
-  hidden3 = tf.nn.relu(tf.matmul(hidden2, weights["DW3"]) + weights["Db3"])
-
-  # Using sigmoid for the last layer since we're outputting the "probability"
-  # that the data came from p_data
-  hidden4 = tf.nn.sigmoid(tf.matmul(hidden3, weights["DW4"]) + weights["Db4"])
-  return hidden4
+  hidden1 = tf.nn.dropout(tf.nn.relu(tf.matmul(data_merged, weights["DW1"]) + weights["Db1"]), keep_prob)
+  hidden2 = tf.nn.dropout(tf.nn.relu(tf.matmul(hidden1, weights["DW2"]) + weights["Db2"]), keep_prob)
+  hidden3 = tf.nn.sigmoid(tf.matmul(hidden2, weights["DW3"]) + weights["Db3"])
+ 
+  return hidden3
 
 def Generator(data_z):
   # Starts from a Gaussian random with mean = 0, stdv = 0.01 of size 128
   hidden1 = tf.nn.relu(tf.matmul(data_z, weights["GW1"]) + weights["Gb1"])
   hidden2 = tf.nn.relu(tf.matmul(hidden1, weights["GW2"]) + weights["Gb2"])
+  hidden3 = tf.nn.tanh(tf.matmul(hidden2, weights["GW3"]) + weights["Gb3"])
 
-  # Using sigmoid for the last layer since MNIST data is in the range[0,1]
-  hidden3 = tf.nn.sigmoid(tf.matmul(hidden2, weights["GW3"]) + weights["Gb3"])
   return hidden3
 
 data_z = tf.truncated_normal(shape=[batch_size,z_size])
@@ -61,36 +59,45 @@ pred = Discriminator(X)
 fake_pred = Discriminator(data_g)
 
 generator_var_list = [weights[i] for i in ["GW1","Gb1","GW2","Gb2","GW3","Gb3"]]
-discriminator_var_list= [weights[i] for i in ["DW1","Db1","DW2","Db2","DW3","Db3","DW4","Db4"]]
+discriminator_var_list= [weights[i] for i in ["DW1","Db1","DW2","Db2","DW3","Db3"]]
 
-initCriterion = tf.reduce_mean(tf.log(fake_pred))
-initOptim = tf.train.AdamOptimizer(0.001).minimize((-1)*initCriterion, var_list=generator_var_list)
+initCriterion = (-1)*tf.reduce_mean(tf.log(fake_pred))
+initOptim = tf.train.AdamOptimizer(0.0001).minimize(initCriterion, var_list=generator_var_list)
 
-stableCriterionForD = tf.reduce_mean(tf.log(pred) + tf.log(1-fake_pred))
-stableOptForD = tf.train.AdamOptimizer(0.001).minimize((-1)*stableCriterionForD, var_list=discriminator_var_list)
+stableCriterionForD = (-1)*tf.reduce_mean(tf.log(pred) + tf.log(1-fake_pred))
+stableOptForD = tf.train.AdamOptimizer(0.0001).minimize(stableCriterionForD, var_list=discriminator_var_list)
 
 stableCriterionForG = tf.reduce_mean(tf.log(1-fake_pred))
-stableOptForG = tf.train.AdamOptimizer(0.001).minimize(stableCriterionForG, var_list=generator_var_list)
+stableOptForG = tf.train.AdamOptimizer(0.0001).minimize(stableCriterionForG, var_list=generator_var_list)
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   for epoch in range(epochs):
-    num_batches = int(mnist.train.num_examples/(K*batch_size))
+    num_batches = int(mnist.train.num_examples/(batch_size))
     for i in range(num_batches):
       # training ratio D:G = K:1 
       # First train D for K times
-      if epoch == 0 and i < 5 : 
-        #gloss, _, fp = sess.run([initCriterion,initOptim,fake_pred]) 
-        gloss,_,fp = sess.run([stableCriterionForG, stableOptForG,fake_pred])
-        print "[E:%d][B:%d] gloss: %f" % (epoch,i,gloss)
-        print fp
+      for index in range(K):
+        batch_x, _ = mnist.train.next_batch(batch_size)
+        batch_x = 2*batch_x - 1
+        dloss, _, p,fp = sess.run([stableCriterionForD, stableOptForD, pred, fake_pred], feed_dict={X:batch_x, keep_prob: 1})
+
+      if epoch < 2: 
+        gloss,_,fp,g,z = sess.run([initCriterion, initOptim, fake_pred,data_g,data_z], feed_dict={keep_prob:1.0})
+        print "[E:%d][B:%d] gloss : %f" % (epoch,i,gloss)
+
+        for j in range(0, 3):
+          plt.subplot(311 + (j))
+          image = np.reshape(g[j],[28,28])
+          plt.imshow(image, interpolation='nearest', cmap='gray')
+        plt.show()
       else :
-        for index in range(K):
-          batch_x, _ = mnist.train.next_batch(batch_size)
-          dloss, _, p,fp = sess.run([stableCriterionForD, stableOptForD, pred, fake_pred], feed_dict={X:batch_x})
-          print "[E:%d][B:%d][K:%d] dloss: %f" % (epoch,i,index,dloss)
-          print p
-          print fp
-        gloss,_,fp = sess.run([stableCriterionForG, stableOptForG,fake_pred])
-        print "[E:%d][B:%d] gloss: %f" % (epoch,i,gloss)
-        print fp
+        gloss,_,fp,g = sess.run([stableCriterionForG, stableOptForG,fake_pred,data_g], feed_dict={keep_prob:1})
+
+    print "[E: %d] dloss: %f, gloss: %f" %(epoch, dloss, gloss)
+    for j in range(0,9):
+      plt.subplot(331 + (j))
+      image = np.reshape(g[j],[28,28])
+        
+      plt.imshow(image, interpolation='nearest', cmap='gray')
+    plt.show()
